519e8b4e8671f1eb5e76b7e7eb5caa9887a3fe6f
TSlump <tslump@vqcomms.com>
2024-12-03 17:33:31 +0000

    Day 3

---
+++ Days/Day3/Day3.cs
@@ -0,0 +1,130 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Days.Day3;
+
+public class Day3
+{
+    public static void RunPartOne()
+    {
+        var filePath = "Days/Day3/Day3Input.txt";
+        
+        var mulList = new List<string>();
+        var pattern = @"^mul\((-?\d+),(-?\d+)\)$";
+
+        if (File.Exists(filePath))
+        {
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var pointer = 0;
+                var length = 8;
+                
+                while (pointer + length <= line.Length)
+                {
+                    if (Regex.IsMatch(line.Substring(pointer, length), pattern))
+                    {
+                        mulList.Add(line.Substring(pointer, length));
+                        pointer += length;
+                        length = 8;
+                    }
+                    else
+                    {
+                        if (length < 12)
+                        {
+                            length += 1;
+                        }
+                        else
+                        {
+                            pointer += 1;
+                            length = 8;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("File not found: " + filePath);
+        }
+
+        var total = 0;
+        
+        foreach (var mul in mulList)
+        {
+            var nums = mul.Replace("mul(", "").Replace(")", "").Split(',');
+            
+            total += int.Parse(nums[0]) * int.Parse(nums[1]);
+        }
+        
+        Console.WriteLine("total: " + total);
+    }
+    
+    public static void RunPartTwo()
+    {
+        var filePath = "Days/Day3/Day3Input.txt";
+        
+        var mulList = new List<string>();
+        var pattern = @"^mul\((\d{1,3}),(\d{1,3})\)$";
+        var enabled = true;
+
+        if (File.Exists(filePath))
+        {
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var pointer = 0;
+                var length = 8;
+                
+                while (pointer + length <= line.Length)
+                {
+                    if (Regex.IsMatch(line.Substring(pointer, length), pattern))
+                    {
+                        if (enabled)
+                        {
+                            mulList.Add(line.Substring(pointer, length));
+                        }
+                        pointer += length;
+                        length = 8;
+                    }
+                    else if (line.Substring(pointer, 4) == "do()")
+                    {
+                        enabled = true;
+                        pointer += 4;
+                        length = 8;
+                    }
+                    else if (line.Substring(pointer, 7) == "don't()")
+                    {
+                        enabled = false;
+                        pointer += 7;
+                        length = 8;
+                    }
+                    else
+                    {
+                        if (length < 12)
+                        {
+                            length += 1;
+                        }
+                        else
+                        {
+                            pointer += 1;
+                            length = 8;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("File not found: " + filePath);
+        }
+
+        var total = 0;
+        
+        foreach (var mul in mulList)
+        {
+            var nums = mul.Replace("mul(", "").Replace(")", "").Split(',');
+            
+            total += int.Parse(nums[0]) * int.Parse(nums[1]);
+        }
+        
+        Console.WriteLine("new total: " + total);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode2024.Days.Day1;
 using AdventOfCode2024.Days.Day2;
+using AdventOfCode2024.Days.Day3;
 
 Console.WriteLine("Advent of Code-2024 \n");
 
@@ -8,3 +9,7 @@ Day1.Run();
 
 Console.WriteLine("\nDay 2\n");
 Day2.Run();
+
+Console.WriteLine("\nDay 3\n");
+Day3.RunPartOne();
+Day3.RunPartTwo();
